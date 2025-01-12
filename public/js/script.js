e34afec575bf378d57b1200d20d2130067e4a72d
Bruno Apolinário <bruno.apolinario010@gmail.com>
2025-01-12 19:21:31 -0300

    feat: add new task to in memory storage

--- public/js/script.js
+++ public/js/script.js
@@ -5,4 +5,27 @@ $(document).ready(function () {
         $('body').css('backgroundColor', color);
         localStorage.setItem('backgroundColor', color);
     });
+
+    let tasks = [];
+
+    const logTasks = () => {
+        console.log(tasks);
+    }
+
+    $('.task-input').on('keypress', function (e) {
+        if (e.key === 'Enter' && $(this).val().trim()) {
+            const task = {
+                id: tasks.length + 1,
+                description: $(this).val().trim(),
+                completed: false,
+                priority: 'none',
+                observations: '',
+                expanded: false
+            };
+
+            tasks.push(task);
+            $(this).val('');
+            logTasks();
+        }
+    });
 });
